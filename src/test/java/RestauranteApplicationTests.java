46af2a659fce2c907e36a3f4d004037297ed5ce8
Igor Encarnação Junqueira Castro <igorjunqueira19@gmail.com>
2025-03-20 09:34:40 -0300

    criando o teste de usuario service

--- src/test/java/RestauranteApplicationTests.java
+++ src/test/java/RestauranteApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class RestauranteApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
